f3cf68c980103f5a6d1b2be81721f33308774322
Tu Nguyen <tanguyen17@wabash.edu>
2017-02-10 20:22:19 +0000

    Some update to Pixy_2

--- pixy_2.py
+++ pixy_2.py
@@ -1,28 +1,51 @@
+'''
+PIXY spec:  640x400 50fps
+'''
+
 from BrickPi import *
 
-I2C_PORT = PORT_1
-I2C_ADDR = 0x02
-I2C_DEVICE_INDEX = 0
+class Pixy(object):
+    def __init__(self, I2C_PORT = PORT_1):
+        self.PORT = PORT_1
+        I2C_ADDR = 0x02
+        self.I2C_DEVICE_INDEX = 0
 
-I2C_REG = 0x43
+        BrickPiSetup()
+        BrickPi.SensorType[self.PORT] = TYPE_SENSOR_I2C
+        BrickPi.SensorI2CSpeed   [self.PORT]    = 0
+        BrickPi.SensorI2CDevices [self.PORT]    = 1
+        BrickPi.SensorI2CAddr    [self.PORT][self.I2C_DEVICE_INDEX] = I2C_ADDR
 
-BrickPiSetup()
-BrickPi.SensorType[I2C_PORT] = TYPE_SENSOR_I2C
-BrickPi.SensorI2CSpeed   [I2C_PORT]    = 0
-BrickPi.SensorI2CDevices [I2C_PORT]    = 1
-BrickPi.SensorI2CAddr    [I2C_PORT][I2C_DEVICE_INDEX] = I2C_ADDR
+    def val(self):
+        BrickPi.SensorSettings [self.PORT][self.I2C_DEVICE_INDEX]    = 0
+        BrickPi.SensorI2CWrite [self.PORT][self.I2C_DEVICE_INDEX]    = 1
+        BrickPi.SensorI2CRead  [self.PORT][self.I2C_DEVICE_INDEX]    = 6
+        BrickPi.SensorI2COut   [self.PORT][self.I2C_DEVICE_INDEX][0] = 0x42
 
+        if(BrickPiSetupSensors()):
+          print "BrickPiSetupSensors failed"
+          sys.exit( 0 )
 
-BrickPi.SensorSettings [I2C_PORT][I2C_DEVICE_INDEX]    = 0
-BrickPi.SensorI2CWrite [I2C_PORT][I2C_DEVICE_INDEX]    = 1
-BrickPi.SensorI2CRead  [I2C_PORT][I2C_DEVICE_INDEX]    = 6
-BrickPi.SensorI2COut   [I2C_PORT][I2C_DEVICE_INDEX][0] = 0x42
+        while True:
+            if(not BrickPiUpdateValues()):
+                if(BrickPi.Sensor[self.PORT] & (0x01 << self.I2C_DEVICE_INDEX)):
+                    n_object = BrickPi.SensorI2CIn[self.PORT][self.I2C_DEVICE_INDEX][0]
+                    object_color = BrickPi.SensorI2CIn[self.PORT][self.I2C_DEVICE_INDEX][4]
+                    x_ul = BrickPi.SensorI2CIn[self.PORT][self.I2C_DEVICE_INDEX][1]
+                    y_ul = BrickPi.SensorI2CIn[self.PORT][self.I2C_DEVICE_INDEX][2]
+                    x_lr = BrickPi.SensorI2CIn[self.PORT][self.I2C_DEVICE_INDEX][3]
+                    y_lr = BrickPi.SensorI2CIn[self.PORT][self.I2C_DEVICE_INDEX][4]
 
-if(BrickPiSetupSensors()):
-  print "BrickPiSetupSensors failed"
-  sys.exit( 0 )
+                    X = (x_ul + x_lr)/2
+                    Y = (y_ul + y_lr)/2
 
-while True:
-    if(not BrickPiUpdateValues()):
-        if(BrickPi.Sensor[I2C_PORT] & (0x01 << I2C_DEVICE_INDEX)):
-            print BrickPi.SensorI2CIn[I2C_PORT][I2C_DEVICE_INDEX][0:4]
+                    if n_object > 0:
+                        print 'X: ', X, '- Y: ', Y, " Color: ", object_color
+            time.sleep(0.02)
+
+def main():
+    p = Pixy()
+    p.val()
+
+if __name__ == '__main__':
+    main()
